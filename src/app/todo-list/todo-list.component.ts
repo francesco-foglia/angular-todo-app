d88bf896d71dd015382507d7cf9e8aa29454b170
Francesco Foglia <francescofogliajj@gmail.com>
2022-10-21 16:17:53 +0200

    modified:   src/app/app.component.html 	modified:   src/app/todo-list/todo-list.component.css 	modified:   src/app/todo-list/todo-list.component.html 	modified:   src/app/todo-list/todo-list.component.ts

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -6,8 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
+  todos: any[];
 
-  constructor() { }
+  constructor() {
+    this.todos = [
+      { id: 1, desc: 'Task 1', date: new Date(), priority: 2, completed: false },
+      { id: 2, desc: 'Task 2', date: new Date(), priority: 5, completed: false },
+      { id: 3, desc: 'Task 3', date: new Date(), priority: 1, completed: false },
+      { id: 4, desc: 'Task 4', date: new Date(), priority: 5, completed: false },
+      { id: 5, desc: 'Task 5', date: new Date(), priority: 1, completed: false },
+    ];
+  }
 
   ngOnInit(): void {
   }
